a84e158a2a57949cf320d8740c09f86eee5402e5
jcsantos23 <jcsantos23@gmail.com>
2016-01-29 12:51:24 -0800

    created first-link branch and First.php controller

---
+++ application/controllers/First.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * The first after the logo in the menu bar.
+ * The page should display the first item in the list with their picture and quote.
+ * 
+ * controllers/First.php
+ *
+ * ------------------------------------------------------------------------
+ */
+class First extends Application {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    //-------------------------------------------------------------
+    //  The normal pages
+    //-------------------------------------------------------------
+
+    function index() {
+        $this->data['pagebody'] = 'justone'; // this is the view we want shown
+        // grab the first item in the list, to pass on to our view
+        $record = $this->quotes->first();
+
+        $this->data = array_merge($this->data, $record);
+
+        $this->render();
+    }
+
+}
+
+/* End of file First.php */
+/* Location: application/controllers/First.php */
