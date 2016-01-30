9e9780d637a7e914d0c5e4bc0e9253e250ba5dcb
johna <johna@DESKTOP-U77VMO7>
2016-01-30 15:41:29 -0800

    added wise.php

---
+++ application/controllers/Wise.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Our homepage. Show a table of all the author pictures. Clicking on one should show their quote.
+ * Our quotes model has been autoloaded, because we use it everywhere.
+ * 
+ * controllers/Welcome.php
+ *
+ * ------------------------------------------------------------------------
+ */
+class Wise extends Application {
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	//-------------------------------------------------------------
+	//  The normal pages
+	//-------------------------------------------------------------
+
+	function bingo()
+	{
+        $this->data['pagebody'] = 'justone'; // this is the view we want shown
+        // grab the sixth item in the list, to pass on to our view
+        $record = $this->quotes->get(6);
+
+        $this->data = array_merge($this->data, $record);
+
+        $this->render();
+    }
+
+}
+
+/* End of file Welcome.php */
+/* Location: application/controllers/Welcome.php */
